27d3941305c45d3ac39e0f6e404d605c7d4bfbee
Carlos Henrique <krloss@github.com>
2017-06-19 07:19:37 -0300

    inicio

---
+++ projeto/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projeto::Application.config.secret_token = 'b8b8647cbf3802e80ee32cec23fa15fc06f4b5a2922ff63a3d5cd0bcd8f205b0f7ab90ced5d55e2ddd5536d27aaf4932bc6a88f8c2ba0e7d98afba5832434a16'
